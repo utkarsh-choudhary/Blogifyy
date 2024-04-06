d2fdf505c80e9c68334bfc90783796d25f467845
utkarsh-choudhary <utkarshchoudhary734@gmail.com>
2024-04-06 22:54:02 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require("dotenv").config()
+
+const path=require("path");
+const express=require("express");
+const mongoose=require("mongoose");
+
+const cookieParser=require("cookie-parser");
+
+const Blog=require("./models/blog")
+
+const userRoute=require("./routes/user");
+const blogRoute=require("./routes/blog");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
+
+
+
+const app=express();
+const PORT=process.env.PORT || 8000;
+
+mongoose.connect(process.env.MONGO_URL).then((e)=>{
+    console.log("mongodb connected");
+})
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser()); 
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+
+app.get("/", async(req,res)=>{
+    const allBlogs=await Blog.find({});
+    res.render("home",{
+        user:req.user,
+        blogs:allBlogs,
+    });
+});
+
+app.use("/user",userRoute);
+app.use("/blog",blogRoute);
+
+
+app.listen(PORT,()=> console.log(`server started at port: ${PORT}`));
